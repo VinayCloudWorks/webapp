a8b77e4a41b1faa45b6d6643e0e4b2ca7ebadecd
Vinay Sathe <sathe.v@northeastern.edu>
2025-03-18 22:29:16 -0400

    DB Schema updated

--- models/file.js
+++ models/file.js
@@ -3,20 +3,20 @@ const { DataTypes } = require('sequelize');
 const { sequelize } = require('../utils');
 
 const File = sequelize.define('File', {
-    file_id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true
-    },
     file_name: {
         type: DataTypes.STRING,
         allowNull: false
     },
-    s3_bucket_path: {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    url: {
         type: DataTypes.STRING,
         allowNull: false
     },
-    date_created: {
+    upload_date: {
         type: DataTypes.DATE,
         defaultValue: DataTypes.NOW
     }
